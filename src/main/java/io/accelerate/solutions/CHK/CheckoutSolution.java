1e05cb97291f1ed00057c56245c03ee8f4a44b37
sbx_user1051 <sbx_user1051@169.254.54.183>
2025-05-17 18:05:25 +0000

    Sat May 17 18:05:25 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -54,13 +54,28 @@ public class CheckoutSolution {
 
         // Apply buy two E get one B free
         int newBCount = Math.max(0, cart.get('B') - cart.get('E') / 2);
-        System.out.println(cart.get('E') + "E have been bought, number of chargeable B goes from " + cart.get('B') + " to " + newBCount);
+        //System.out.println(cart.get('E') + "E have been bought, number of chargeable B goes from " + cart.get('B') + " to " + newBCount);
         cart.put('B', newBCount);
 
         // Apply buy two F get one F free
         int newFCount = cart.get('F') - cart.get('F') / 3;
-        System.out.println(cart.get('F') + "F have been bought, number of chargeable F goes from " + cart.get('F') + " to " + newFCount);
+        //System.out.println(cart.get('F') + "F have been bought, number of chargeable F goes from " + cart.get('F') + " to " + newFCount);
         cart.put('F', newFCount);
+
+        // Apply buy three N get one M free
+        int newMCount = Math.max(0, cart.get('M') - cart.get('N') / 3);
+        //System.out.println(cart.get('N') + "N have been bought, number of chargeable M goes from " + cart.get('M') + " to " + newMCount);
+        cart.put('M', newMCount);
+
+        // Apply buy three R get one Q free
+        int newQCount = Math.max(0, cart.get('Q') - cart.get('R') / 3);
+        //System.out.println(cart.get('R') + "R have been bought, number of chargeable Q goes from " + cart.get('Q') + " to " + newQCount);
+        cart.put('Q', newQCount);
+
+        // Apply buy two U get one F free
+        int newUCount = cart.get('U') - cart.get('U') / 3;
+        //System.out.println(cart.get('U') + "U have been bought, number of chargeable U goes from " + cart.get('U') + " to " + newUCount);
+        cart.put('U', newUCount);
 
         int aCount = cart.get('A');
         int bCount = cart.get('B');
@@ -89,15 +104,59 @@ public class CheckoutSolution {
         int yCount = cart.get('Y');
         int zCount = cart.get('Z');
 
-
-
         int aItemsPrice = ((aCount / 5) * 200) + (((aCount % 5) / 3) * 130) + (((aCount % 5) % 3) * 50);
         int bItemsPrice = ((bCount / 2) * 45) + ((bCount % 2) * 30);
         int cItemsPrice = cCount * 20;
         int dItemsPrice = dCount * 15;
         int eItemsPrice = eCount * 40;
         int fItemsPrice = fCount * 10;
+        int gItemsPrice = gCount * 20;
+        int hItemsPrice = ((hCount / 10) * 80) + (((hCount % 10) / 5) * 45) + (((hCount % 10) % 5) * 10);
+        int iItemsPrice = iCount * 35;
+        int jItemsPrice = jCount * 60;
+        int kItemsPrice = ((kCount / 2) * 150) + ((kCount % 2) * 80);
+        int lItemsPrice = lCount * 90;
+        int mItemsPrice = mCount * 15;
+        int nItemsPrice = nCount * 40;
+        int oItemsPrice = oCount * 10;
+        int pItemsPrice = ((pCount / 5) * 200) + ((pCount % 5) * 50);
+        int qItemsPrice = ((qCount / 3) * 80) + ((qCount % 3) * 30);
+        int rItemsPrice = rCount * 50;
+        int sItemsPrice = sCount * 30;
+        int tItemsPrice = tCount * 20;
+        int uItemsPrice = uCount * 40;
+        int vItemsPrice = ((vCount / 3) * 130) + (((vCount % 3) / 2) * 90) + (((vCount % 3) % 2) * 50);
+        int wItemsPrice = wCount * 20;
+        int xItemsPrice = xCount * 90;
+        int yItemsPrice = yCount * 10;
+        int zItemsPrice = zCount * 50;
 
-        return aItemsPrice + bItemsPrice + cItemsPrice + dItemsPrice + eItemsPrice + fItemsPrice;
+        return aItemsPrice +
+               bItemsPrice +
+               cItemsPrice +
+               dItemsPrice +
+               eItemsPrice +
+               fItemsPrice +
+               gItemsPrice +
+               hItemsPrice +
+               iItemsPrice +
+               jItemsPrice +
+               kItemsPrice +
+               lItemsPrice +
+               mItemsPrice +
+               nItemsPrice +
+               oItemsPrice +
+               pItemsPrice +
+               qItemsPrice +
+               rItemsPrice +
+               sItemsPrice +
+               tItemsPrice +
+               uItemsPrice +
+               vItemsPrice +
+               wItemsPrice +
+               xItemsPrice +
+               yItemsPrice +
+               zItemsPrice;
     }
 }
+
