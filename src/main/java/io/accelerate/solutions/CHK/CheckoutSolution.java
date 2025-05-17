a2dbbcede950cb730cff8de3119675a0c26929f5
sbx_user1051 <sbx_user1051@169.254.54.183>
2025-05-17 16:25:56 +0000

    Sat May 17 16:25:56 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,17 @@ package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class CheckoutSolution {
+
     public Integer checkout(String skus) {
+        HashMap<String, Integer> cart = new HashMap<>();
+
+        // For each letter 
+
         return -1;
     }
 }
+
