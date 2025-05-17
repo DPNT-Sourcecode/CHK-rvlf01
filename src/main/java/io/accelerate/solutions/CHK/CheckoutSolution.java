5c8418e117813af3d71719e9142c421d6739980f
sbx_user1051 <sbx_user1051@169.254.54.183>
2025-05-17 16:43:56 +0000

    Sat May 17 16:43:56 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -8,22 +8,29 @@ import java.util.HashSet;
 public class CheckoutSolution {
 
     public Integer checkout(String skus) {
+        if (skus.isBlank()) {
+            return -1;
+        }
+
         HashMap<Character, Integer> cart = new HashMap<>();
         cart.put("A", 0);
         cart.put("B", 0);
         cart.put("C", 0);
         cart.put("D", 0);
 
-
         for (int i = 0; i < skus.length(); i++) {
+            if (!cart.containsKey(skus.charAt(i))) {
+                return -1;
+            }
             cart.put(skus.charAt(i), cart.get(skus.charAt(i)) + 1);
         }
 
-        int aItemsPrice = cart.
+        int aItemsPrice = ((cart.get("A") / 3) * 130) + ((cart.get("A") % 3) * 50);
+        int bItemsPrice = ((cart.get("B") / 2) * 45) + ((cart.get("B") % 2) * 30);
         int cItemsPrice = cart.get("C") * 20;
         int dItemsPrice = cart.get("D") * 15;
 
-
-        return -1;
+        return aItemsPrice + bItemsPrice + cItemsPrice + dItemsPrice;
     }
 }
+
