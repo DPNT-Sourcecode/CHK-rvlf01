364f46d5e88b0c6b9e09b94e8b26ae80c239e6fb
sbx_user1051 <sbx_user1051@169.254.54.183>
2025-05-17 16:28:56 +0000

    Sat May 17 16:28:56 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -9,10 +9,18 @@ public class CheckoutSolution {
 
     public Integer checkout(String skus) {
         HashMap<String, Integer> cart = new HashMap<>();
+        cart.put("A", 0);
+        cart.put("B", 0);
+        cart.put("C", 0);
+        cart.put("D", 0);
 
-        // For each letter 
+
+        for (int i = 0; i < skus.length(); i++) {
+            cart.get(String.valueOf(skus.charAt(i)))++;
+        }
 
         return -1;
     }
 }
+
 
